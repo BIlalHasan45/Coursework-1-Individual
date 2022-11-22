7fab730765e8fbc3f15943a1a30c687120ae242d
Bilal Hasan <52481490+BIlalHasan45@users.noreply.github.com>
2022-11-22 10:27:56 +0400

    Add files via upload

---
+++ Individual/js/lessons.js
@@ -0,0 +1,110 @@
+let lessons = [
+    {
+        "id": 1001,
+        "subject": 'Web Designing',
+        "location": 'Dubai',
+        "price": 100,
+        "image": "images/web designing.png",
+        "stock": 5
+
+    },
+    {
+        "id": 1002,
+        "subject": 'Web Development',
+        "location": 'Abu Dhabi',
+        "price": 150,
+        "image": "images/web development.jpg",
+        "stock": 6
+
+    },
+    {
+        "id": 1003,
+        "subject": 'User Interface',
+        "location": 'Sharjah',
+        "price": 200,
+        "image": "images/user interface.jpg",
+        "stock": 7
+
+    },
+    {
+        "id": 1004,
+        "subject": 'User Experience',
+        "location": 'Ajman',
+        "price": 250,
+        "image": "images/user experience.png",
+        "stock": 8
+
+    },
+    {
+        "id": 1005,
+        "subject": 'UG Individual Project',
+        "location": 'Qatar',
+        "price": 300,
+        "image": "images/ug individual project.jpg",
+        "stock": 9
+
+    },
+    {
+        "id": 1006,
+        "subject": 'Graphic Designing',
+        "location": 'Oman',
+        "price": 350,
+        "image": "images/graphic designing.jpg",
+        "stock": 10
+
+    },
+    {
+        "id": 1007,
+        "subject": 'Business Intelligence',
+        "location": 'Pakistan',
+        "price": 400,
+        "image": "images/business intelligence.png",
+        "stock": 11
+
+    },
+    {
+        "id": 1008,
+        "subject": 'Artificial Intelligence',
+        "location": 'India',
+        "price": 450,
+        "image": "images/artificial intelligence.png",
+        "stock": 12
+
+    },
+    {
+        "id": 1009,
+        "subject": 'Strategic Information System',
+        "location": 'Saudia',
+        "price": 500,
+        "image": "images/strategic information system.jpg",
+        "stock": 13
+
+    },
+    {
+        "id": 1010,
+        "subject": 'Web Development with Big Data',
+        "location": 'America',
+        "price": 550,
+        "image": "images/web development big data.jpg",
+        "stock": 14
+
+    },
+    {
+        "id": 1011,
+        "subject": 'Digital Media Marketing',
+        "location": 'London',
+        "price": 600,
+        "image": "images/digital media marketing.jpg",
+        "stock": 15
+
+    },
+    {
+        "id": 1012,
+        "subject": 'Mobile App Development',
+        "location": 'France',
+        "price": 650,
+        "image": "images/mobile app development.png",
+        "stock": 16
+
+    },
+]
